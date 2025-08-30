486e40096d18e1f98208cd05bfc71502dc87932c
YASH1825 <148941983+Thestrawhatsuser@users.noreply.github.com>
2025-08-30 19:58:31 +0530

    routing
    page integration using react routing dom

---
+++ K-27/src/pages/agence.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      Agence
+    </div>
+  )
+}
+
+export default home
---
+++ K-27/src/pages/projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      Projects
+    </div>
+  )
+}
+
+export default home
