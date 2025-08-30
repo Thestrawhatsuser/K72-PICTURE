047e0c492345e717e62bd029f5418829634b4507
YASH1825 <148941983+Thestrawhatsuser@users.noreply.github.com>
2025-08-30 23:10:32 +0530

    homepage
    homepage integration with videos

---
+++ K-27/src/components/home/HomeHeroText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeHeroText = () => {
+  return (
+    <div>
+      <div className='font-[font-1]'>The spark for</div>
+      <div>allthings</div>
+      <div>creative</div>
+    </div>
+  )
+}
+
+export default HomeHeroText
---
+++ K-27/src/components/home/video.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const video = () => {
+  return (
+    <div className=''>
+      <video className='h-full w-full object-cover 'autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756573952~exp=1756577552~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=524b69ceb768b439358708a42081bed17438d9b86efc1a790097c684622fd20a&r=dXMtd2VzdDE%3D"></video>
+    </div>
+  )
+}
+
+export default video
