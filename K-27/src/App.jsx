16a4a3f480e0919c856fedeb1db7fd837a22a7ca
YASH1825 <148941983+Thestrawhatsuser@users.noreply.github.com>
2025-09-04 06:35:41 +0530

    animation initialisation
    using gsap for animation

--- K-27/src/App.jsx
+++ K-27/src/App.jsx
@@ -4,10 +4,50 @@ import Agence from './pages/agence'
 import Projects from './pages/projects'
 import { Route , Routes } from 'react-router-dom'
 import './index.css'
-
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
 const App = () => {
+
+useGSAP(function(){
+
+  const tl = gsap.timeline()
+
+
+
+  tl.from('.stair',{
+    scaleY: 0,  
+    transformOrigin: "top",// Instead of just scaleY
+    duration: 0.5,
+    stagger: {
+      each: 0.1
+      
+    }
+  })
+  tl.to('.stair',{
+      y: '100%',
+      duration: 0.5,
+      stagger: {
+        each: 0.1}
+  })
+
+
+})
+
+
+
   return (
-    <div className='text-white'>
+    <div className='text-white'> 
+
+   <div className='stair h-screen w-full fixed z-20 top-0 flex '>
+   <div className='stair h-full w-1/5 bg-black'> </div>
+   <div className='stair h-full w-1/5 bg-black'> </div>
+   <div className='stair h-full w-1/5 bg-black'> </div>
+   <div className='stair h-full w-1/5 bg-black'> </div>
+   <div className='stair h-full w-1/5 bg-black'> </div>
+         
+
+    </div>
+
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/agence' element={<Agence/>}/>
--- K-27/src/pages/agence.jsx
+++ K-27/src/pages/agence.jsx
@@ -1,12 +1,74 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+const Agence = () => {
+  
 
-const home = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+  const imageArray =
+['https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
+' https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/joel_640X960_3-640x960.jpg',] 
+
+useGSAP(function(){
+    gsap.to(imageDivRef.current,{
+      scrollTrigger:{
+        trigger:imageDivRef.current,
+        markers:false,
+        start:"top 23.5%",
+        end:"top -70%",
+        scrub:true,
+        pin:true,
+        onUpdate:function(elem){
+          let imageIndex;
+          if(elem.progress < 1){
+           imageIndex = Math.floor(elem.progress * imageArray.length)
+          }else{
+           imageIndex = imageArray.length -1
+          }
+          imageRef.current.src = imageArray[imageIndex]
+        }
+        
+  }
+  })
+})
+
   return (
-    <div className='font-[font-2]'>
-     SEVEN7Y <br />
-TWO
+   <div>
+    <div ref={imageDivRef} className='absolute h-[20vw] rounded-[3xl] w-[15vw]  top-40  left-[30vw] '>
+      <img ref={imageRef}  src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt=""/>
     </div>
+     <div className='relative font-[font-1]'>
+      <div className='mt-[55vh]'>
+        <h1 className='text-[20vw] text-center leading-[18vw]'>
+          SEVEN7Y <br />
+          TWO
+        </h1>
+      </div>
+    <div className='pl-[40%] mt-4'>
+      <p className= 'text-4xl'>
+         &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.
+      </p>
+    </div>
+    </div>
+   </div>
+
   )
 }
 
-export default home
+export default Agence
